492651e3a2a3cdbcc142c6ae4953bce5c493577a
DarkSharpness <76582120+DarkSharpness@users.noreply.github.com>
2025-04-06 16:32:54 +0900

    [Feature] A naive C++ kernel for transferring KVcache from/to CPU (#5098)

--- sgl-kernel/python/sgl_kernel/kvcacheio.py
+++ sgl-kernel/python/sgl_kernel/kvcacheio.py
@@ -42,3 +42,47 @@ def transfer_kv_all_layer(
         dst_layer_offset,
         block_quota,
     )
+
+
+def transfer_kv_to_cpu_all_layer_naive(
+    host_indices: torch.Tensor,
+    host_k_buffer: torch.Tensor,
+    host_v_buffer: torch.Tensor,
+    device_indices: torch.Tensor,
+    device_k_buffer: torch.Tensor,
+    device_v_buffer: torch.Tensor,
+    page_size: int,
+    layer_num: int,
+):
+    torch.ops.sgl_kernel.transfer_kv_to_cpu_all_layer_naive(
+        host_indices,
+        host_k_buffer,
+        host_v_buffer,
+        device_indices,
+        device_k_buffer,
+        device_v_buffer,
+        page_size,
+        layer_num,
+    )
+
+
+def transfer_kv_to_gpu_per_layer_naive(
+    host_indices: torch.Tensor,
+    host_k_buffer: torch.Tensor,
+    host_v_buffer: torch.Tensor,
+    device_indices: torch.Tensor,
+    device_k_buffer: torch.Tensor,
+    device_v_buffer: torch.Tensor,
+    page_size: int,
+    layer_id: int,
+):
+    torch.ops.sgl_kernel.transfer_kv_to_gpu_per_layer_naive(
+        host_indices,
+        host_k_buffer,
+        host_v_buffer,
+        device_indices,
+        device_k_buffer,
+        device_v_buffer,
+        page_size,
+        layer_id,
+    )
